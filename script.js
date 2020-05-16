dcf5b0bf9b6957a3e37e8ce1dad01330a90082d4
Ayush23Dash <ayushshanker23@gmail.com>
2020-05-16 17:13:17 +0530

    Add typing effect

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ $('.slider').ripples({
   
 });
 });
+
+var myText = "Launching Soon ...";
+var myArray = myText.split("");
+var loopTimer;
+function frameLooper()
+{
+  if(myArray.length>0)
+  {
+    document.getElementById("type_text").innerHTML += myArray.shift();
+  }else{
+    clearTimeout(loopTimer);
+    return false;
+  }
+  loopTimer = setTimeout('frameLooper()',130);
+}
+frameLooper();
